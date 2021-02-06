dc4eea45901d14051f53c5a4d3a7488fb80d7b60
Ryan Vitter <ryanvitter@gmail.com>
2021-02-06 15:57:28 -0500

    remove vue resource

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import './main.css'
 
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 
 import App from './App.vue'
 
 import store from './store'
-
-Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
